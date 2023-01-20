e237e95c9884e57a9ff0ca24fadb6b87c17f4ed4
Famá Román <famaroman@gmail.com>
2023-01-20 06:41:21 -0300

    add routes and developed signup

--- micro-services/users/express/routes.js
+++ micro-services/users/express/routes.js
@@ -1,7 +1,18 @@
+const { validateSignup } = require("./validators");
+const CUsers = require("../databases/controller_users");
 const router = require("express").Router();
 
-router.post("/signup", (rq, rs) => {
-  rs.send(rq.body);
+router.post("/signup", validateSignup, async (rq, rs) => {
+  if(!await CUsers.addUser(rq.body)){
+    return rs.send("not")
+  };
+  return rs.send("yes")
 });
+
+router.post("/login", (rq, rs) => {});
+
+router.put("/changedatauser", (rq, rs) => {});
+
+router.delete("/account", (rq, rs) => {});
 
 module.exports = router;
