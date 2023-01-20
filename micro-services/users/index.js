570395e41b64392f0a3cb346a60a2e0932fb926f
Famá Román <famaroman@gmail.com>
2023-01-20 06:43:26 -0300

    improved index

--- micro-services/users/index.js
+++ micro-services/users/index.js
@@ -2,10 +2,6 @@ require("dotenv").config();
 
 const db = require("./databases/schema_database")("accounts");
 
-if (!db.models["users"]) {
-  require("./databases/table_users")(db);
-}
-
 const app = require("./express/declared-express");
 
 app.listen(app.get("port"), () => {
