702b49c37bf364173e666a8c2880479b8e0d53e3
manialCode <juanmartinmonti4@gmail.com>
2023-01-25 21:54:09 -0300

    Routes bug: Fixed

--- app-web/src/app.vue
+++ app-web/src/app.vue
@@ -1,26 +1,23 @@
 <script setup>
 import { ref, computed } from "vue";
 import routes from "./routes.js";
-import error_404 from './components/error_404.vue'
+import error_404 from "./components/error_404.vue";
 
-
-const currentPath = ref(window.location.hash);
-
+const currentPath = ref(window.location.pathname);
 
 window.addEventListener("hashchange", () => {
-  currentPath.value = window.location.hash;
+  currentPath.value = window.location.pathname;
 });
-
 
 const currentView = computed(() => {
   console.log(currentPath);
-  return routes[currentPath.value.slice(1) || '/'] || error_404
-})
+  return routes[currentPath.value.slice(1) || "/"] || error_404;
+});
 </script>
 
 <template>
   <div>
-    <a href="#/error">throw error 404</a>
+    <a href="/error">throw error 404</a>
     <component :is="currentView" />
   </div>
 </template>
